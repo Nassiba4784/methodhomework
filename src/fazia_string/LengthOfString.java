49af882022e9037c195a9ef8e93bf62c427cae33
faziamebrek <faziamebrek@gmail.com>
2024-09-27 20:45:10 -0700

    lengthOfString

---
+++ src/fazia_string/LengthOfString.java
@@ -0,0 +1,12 @@
+package fazia_string;
+
+public class LengthOfString {public static void main(String[] args) {
+    String str = " open an amazon account needs an email address";
+    int length = str.length();
+    System.out.println("the length of the string is " + length);
+
+}
+}
+
+
+
