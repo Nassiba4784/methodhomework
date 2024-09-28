6a4107c01ecef87e7e4ff57574c2b970d4d09984
faziamebrek <faziamebrek@gmail.com>
2024-09-27 22:58:22 -0700

    WordsCount

---
+++ src/fazia_string/WordsCount.java
@@ -0,0 +1,15 @@
+package fazia_string;
+
+public class WordsCount {
+    public static void main(String[] args) {
+    String str = "i love my family too much";
+    String arr [] = str.split(" ");
+    System.out.println(" The number of words: " + arr.length);
+
+}
+
+}
+
+
+
+
